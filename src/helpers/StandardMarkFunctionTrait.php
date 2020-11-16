06906a9c0fb24fd18cb3eeee176794856b8ebe60
aram <aram.ds@gmail.com>
2020-11-17 01:51:20 +0400

    Add: new marker trait

---
+++ src/helpers/StandardMarkFunctionTrait.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace devnullius\helper\helpers;
+
+/**
+ * Trait StandardMarkFunctionTrait
+ *
+ * @package devnullius\helper\helpers
+ */
+trait StandardMarkFunctionTrait
+{
+
+    public function markAsDeleted(string $fieldName = 'deleted'): void
+    {
+        $this->markPositive($fieldName);
+    }
+
+    private function markPositive(string $fieldName): void
+    {
+        $this->{$fieldName} = true;
+    }
+
+    public function markAsNotDeleted(string $fieldName = 'deleted'): void
+    {
+        $this->markNegative($fieldName);
+    }
+
+    private function markNegative(string $fieldName): void
+    {
+        $this->{$fieldName} = false;
+    }
+
+    public function markAsArchived(string $fieldName = 'archived'): void
+    {
+        $this->markPositive($fieldName);
+    }
+
+    public function markAsNotArchived(string $fieldName = 'archived'): void
+    {
+        $this->markNegative($fieldName);
+    }
+
+    public function markAsEnabled(string $fieldName = 'is_enabled'): void
+    {
+        $this->markPositive($fieldName);
+    }
+
+    public function markAsDisabled(string $fieldName = 'is_enabled'): void
+    {
+        $this->markNegative($fieldName);
+    }
+
+    public function markAsPublished(string $fieldName = 'is_published'): void
+    {
+        $this->markPositive($fieldName);
+    }
+
+    public function markAsUnPublished(string $fieldName = 'is_published'): void
+    {
+        $this->markNegative($fieldName);
+    }
+}
